132c6e20493edc7ec20d615c132b1410e1e5e693
Lijun Zhu <gatechzhu@gmail.com>
2017-02-14 14:37:56 -0500

    Transfer files from ricker repo. Add a README.md

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# wiggle
-Wiggle plot for seismic section data.
+# Ricker Wavelet Generator
+
+[![Build Status](https://travis-ci.org/gatechzhu/wiggle.svg?branch=master)](https://travis-ci.org/gatechzhu/wiggle)
+
+## Introduction
+The [wiggle](http://wiki.aapg.org/Seismic_data_display) display is an ingenious methodology that displays two dimensional scalar fields on a horizontal plane. 
+Originally developed by the geophysical community, the wiggle plot was created to provide a visual analysis of seismic and seismological data, or any other vibration data, in order to help the identification of events that can be stressed out with the coherent alignment of lobes. 
+Ultimately those events can be related to geological features and/or can help the determination of the some physical properties of rocks, such as the velocity of P and S waves. 
+Before digital displays were standard in the industry the wiggle plot was composed either by oscillatory continuous lines and black filled lobes, both drawn by special plotters on long paper sheets. 
+Nowadays, when digital graphical displays are easily available, both elements, the lines and the lobes, are merged into a new one display called wiggle.
+
+Inspired by [wiggle in Matlab](https://www.mathworks.com/matlabcentral/fileexchange/38691-wiggle) function, I created this Python tools to mimic the experience of plotting seismic section data in [Matlab](https://www.mathworks.com/products/matlab.html) with similar user interface. 
+Basically one can control the color and direction of the lines, the color of the left and right lobes, among others. 
+In order to control these features, a controlling string must be provided as input, in a similar way the function PLOT allows control of the graphical elements. 
+
+Given a d M x N ndarray data matrix D, wiggle decompose it into multiple 
+traces. 
+Under vertical mode (default), each columns is a seismic trace of size M and 
+there are N number of traces. 
+When horizontal mode is activated, each row is considered a trace of size N 
+and there are M number of traces.
+
+
+## Dependancy
+- [NumPy](http://www.numpy.org/)
+- [Matplotlib](http://matplotlib.org/)
+
+## Installation
+### From PyPI
+```
+pip install wiggle
+```
+
+### From source file
+Download srouce file from [releases page](https://github.com/gatechzhu/wiggle/releases). Under the root directory, type:
+
+```
+python setup.py install
+```
+
+## Contact
+
+In counter of any trouble, contact *gatechzhu@gmail.com*
---
+++ setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+"""Ricker Wavelet Generator for Seismic Simulation
+
+This is a Ricker wavelet generator for a shifted Ricker wavelet signal
+seismic simulation. It depends on the NumPy package which gives a nice
+matrix/array structure to represent data.
+"""
+
+DOCLINES = __doc__.split("\n")
+
+import sys
+
+from setuptools import setup
+
+# Check Python version
+# if sys.version_info[:2] < (2, 6) or (3, 0) <= sys.version_info[0:2] < (3, 2):
+#     raise RuntimeError("Python version 2.6, 2.7 or >= 3.2 required.")
+if sys.version_info < (3, 4):
+    raise RuntimeError("Python version >= 3.4 required.")
+
+CLASSIFIERS = """\
+Development Status :: 2 - Pre-Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Software Development
+Topic :: Scientific/Engineering
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: MacOS
+
+"""
+
+MAJOR = 0
+MINOR = 1
+MICRO = 0
+ISRELEASED = False
+VERSION = '{}.{}.{}'.format(MAJOR, MINOR, MICRO)
+
+
+# VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+def setup_package():
+    # Figure out whether to add ``*_requires = ['numpy']``.
+    # We don't want to do that unconditionally, because we risk updating
+    # an installed numpy which fails too often.  Just if it's not installed, we
+    # may give it a try.  See gh-3379.
+    try:
+        import numpy
+    except ImportError:  # We do not have numpy installed
+        build_requires = ['numpy>=1.8.2']
+    else:
+        # If we're building a wheel, assume there already exist numpy wheels
+        # for this platform, so it is safe to add numpy to build requirements.
+        # See gh-5184.
+        build_requires = (['numpy>=1.8.2'] if 'bdist_wheel' in sys.argv[1:]
+                          else [])
+
+    metadata = dict(
+        name='wiggle',
+        maintainer="Lijun Zhu",
+        version=VERSION,
+        maintainer_email="gatechzhu@gmail.com",
+        description=DOCLINES[0],
+        long_description="\n".join(DOCLINES[2:]),
+        url="https://github.com/gatechzhu/wiggle",
+        download_url="https://github.com/gatechzhu/wiggle/releases",
+        license='MIT',
+        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+        platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+        test_suite='tests',
+        setup_requires=build_requires,
+        install_requires=build_requires,
+        tests_require=build_requires + ['pytest'],
+    )
+
+    setup(**metadata)
+
+
+if __name__ == '__main__':
+    setup_package()
