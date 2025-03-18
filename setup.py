7ae6fa59ddbaa388574adbdcded032eb4b7a708c
Lijun Zhu <gatechzhu@gmail.com>
2025-03-17 21:01:09 -0500

    REF: update setup.cfg to specify Python version requirements and remove version check from setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-import sys
 from setuptools import setup
-
-# Check Python version: require 2.7 or >= 3.4
-if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[:2] < (3, 4):
-    sys.exit("Python version 2.7 or >= 3.4 required.")
 
 setup()  # Reads configuration from setup.cfg
